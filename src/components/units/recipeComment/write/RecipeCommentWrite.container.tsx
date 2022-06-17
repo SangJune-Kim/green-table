c4a1f980d0989d83de7fccd4242f3e75d0650d69
Kingmo <55ooccmm@gmail.com>
2022-06-17 15:49:21 +0900

    Modify: [#62] 댓글 수정 구현 중..

--- src/components/units/recipeComment/write/RecipeCommentWrite.container.tsx
+++ src/components/units/recipeComment/write/RecipeCommentWrite.container.tsx
@@ -1,6 +1,7 @@
 import RecipeCommentWriteUI from "./RecipeCommentWrite.presenter";
 import {
   CREATE_REPLY,
+  UPDATE_REPLY,
   FETCH_REPLIES,
   FETCH_RECIPE,
 } from "./RecipeCommentWrite.queries";
@@ -14,6 +15,7 @@ export default function RecipeCommentWrite(props: IRecipeCommentWriteProps) {
   const router = useRouter();
   const { Success, Warning, ModalError } = useModal();
   const [createReply] = useMutation(CREATE_REPLY);
+  const [updateReply] = useMutation(UPDATE_REPLY);
   const { register, handleSubmit, setValue } = useForm<FieldValues, any>({
     mode: "onChange",
   });
@@ -43,12 +45,40 @@ export default function RecipeCommentWrite(props: IRecipeCommentWriteProps) {
       if (error instanceof Error) ModalError("등록 실패", error.message);
     }
   };
+  // 댓글 수정하기
+  const onClickUpdate = async (data: any) => {
+    try {
+      await updateReply({
+        variables: {
+          reply_id: props.el?.reply_id,
+          recipe_id: String(router.query.recipeId),
+          contents: data.contents,
+        },
+        refetchQueries: [
+          {
+            query: FETCH_REPLIES,
+            variables: { id: String(router.query.recipeId) },
+          },
+          {
+            query: FETCH_RECIPE,
+            variables: { recipes_id: String(router.query.recipeId) },
+          },
+        ],
+      });
+    } catch (error) {
+      if (error instanceof Error) ModalError("수정 실패", error.message);
+    }
+  };
+  console.log(props?.el);
 
   return (
     <RecipeCommentWriteUI
+      isEdit={props.isEdit}
+      el={props.el}
       register={register}
       handleSubmit={handleSubmit}
       onClickSubmit={onClickSubmit}
+      onClickUpdate={onClickUpdate}
     />
   );
 }
